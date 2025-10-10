30e0120955a5891a4c094441baaa86602ff72550
Romit Dasgupta <98577369+romitdasgupta@users.noreply.github.com>
2025-10-09 22:00:23 -0700

    Add back some of the commented out serial logging

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -55,20 +55,20 @@ extern "C" void kernel_main(uint32_t multiboot_magic, uint32_t multiboot_info) {
     vga.put_hex(multiboot_magic);
     vga.putchar('\n');
 
-    // serial.puts("Multiboot Magic: ");
-    // serial.put_hex(multiboot_magic);
-    // serial.putchar('\n');
+    serial.puts("Multiboot Magic: ");
+    serial.put_hex(multiboot_magic);
+    serial.putchar('\n');
 
-    // if (multiboot_magic != 0x36d76289) {
-    //     vga.set_color(VGA::LIGHT_RED, VGA::BLACK);
-    //     vga.puts("ERROR: Invalid multiboot magic!\n");
-    //     serial.puts("ERROR: Invalid multiboot magic!\n");
-    //     while(1) asm volatile("hlt");
-    // }
+    if (multiboot_magic != 0x36d76289) {
+        vga.set_color(VGA::LIGHT_RED, VGA::BLACK);
+        vga.puts("ERROR: Invalid multiboot magic!\n");
+        serial.puts("ERROR: Invalid multiboot magic!\n");
+        while(1) asm volatile("hlt");
+    }
 
     vga.set_color(VGA::LIGHT_GREEN, VGA::BLACK);
     vga.puts("Multiboot OK\n\n");
-    // serial.puts("Multiboot OK\n\n");
+    serial.puts("Multiboot OK\n\n");
     
     // Print CPU information
     vga.set_color(VGA::WHITE, VGA::BLACK);
@@ -76,13 +76,13 @@ extern "C" void kernel_main(uint32_t multiboot_magic, uint32_t multiboot_info) {
 
     // Check for VMX
     vga.putchar('\n');
-    // serial.putchar('\n');
+    serial.putchar('\n');
     if (!CPUID::has_vmx()) {
         vga.set_color(VGA::LIGHT_RED, VGA::BLACK);
         vga.puts("ERROR: VMX not supported by CPU!\n");
         vga.puts("Cannot proceed with hypervisor initialization.\n");
-        // serial.puts("ERROR: VMX not supported by CPU!\n");
-        // serial.puts("Cannot proceed with hypervisor initialization.\n");
+        serial.puts("ERROR: VMX not supported by CPU!\n");
+        serial.puts("Cannot proceed with hypervisor initialization.\n");
         while(1) asm volatile("hlt");
     }
 
